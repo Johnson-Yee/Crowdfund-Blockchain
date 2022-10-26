405998f6d1c3142869e2361b76c01e97d56c9769
soepaingzaw <soepaing6c2@gmail.com>
2022-10-26 22:20:48 +0800

    Added functions

--- contracts/Crowdfund.sol
+++ contracts/Crowdfund.sol
@@ -1,28 +1,43 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.6.0) (token/ERC20/IERC20.sol)
 pragma solidity ^0.8.0;
 
-import "./IERC20.sol";
+//import "./IERC20.sol";
 
 contract Crowdfund {
     event StartCampaign(uint id, address creator, uint goal, uint startTime, uint endTime);
     event DropCampaign(uint id);
+
+    event Donate(uint indexed _id, address indexed caller,uint amount);
+    event Withdraw(uint indexed _id,address indexed caller,uint amount);
+
+    event ProcessFunds(uint id);
+    event Reimburse(uint id, address indexed caller, uint amount);
+
 
     struct Campaign {
         address creator;
         uint minContribution; //in weis
         uint currentAmount;
         uint goal;
+        uint balance;
         uint32 startTime;
         uint32 endTime;
         bool claimed;
     }
+
+    /*
     IERC20 public immutable token;
+    */
     uint public campaignCount;
     mapping(uint=> Campaign) public ongoingCampaigns; // map count to campaign e.g 0 => Campaign object
     mapping(uint=> mapping(address=>uint)) public potentialDonations; // map count to dict e.g 0 => {address1:100wei, ...., addressN:200wei}
 
-    constructor(address _creator) {
-        token = IERC20(_creator);
+    /*
+    constructor(address _token) {
+        token = IERC20(_token);
     }
+    */
 
     function startCampaign(uint _goal, uint32 _startTime, uint32 _endTime) external {
         require(_startTime >= block.timestamp, "start must be after block creation");
@@ -33,6 +48,7 @@ contract Crowdfund {
             minContribution: 100,
             currentAmount: 0,
             goal: _goal,
+            balance: _balance,
             startTime: _startTime,
             endTime: _endTime,
             claimed: false
@@ -50,16 +66,60 @@ contract Crowdfund {
         require(block.timestamp < campaign.startTime, "Campaign has already started");
         delete ongoingCampaigns[_id];
         emit DropCampaign(_id);
+    
+    }
+    
+    function donate(uint _id, uint _amount) external {
+        Campaign storage campaign = ongoingCampaigns[_id];
+        require(block.timestamp >= campaign.startTime, "not started");
+        require(block.timestamp <= campaign.endTime, "ended");
+
+        campaign.currentAmount += _amount;
+        potentialDonations[_id][msg.sender] += _amount;
+        token.transferFrom(msg.sender, address(this), _amount);
+
+        emit Donate(_id, msg.sender, _amount);
     }
 
-    function withdraw(uint amount, address payable destAddr) public {
-       
-        require(msg.sender ==owner, "Only owner can withdraw");
-        require(amount <= balance, "Insufficent funds");
 
-        destAddr.transfer(amount);
-        balance -= amount;
 
+    function withdraw(uint _id, uint _amount) external {
+        Campaign storage campaign = ongoingCampaigns[_id];
+        require(block.timestamp <= campaign.endTime, "ended");
+
+        campaign.currentAmount -= _amount;
+        potentialDonations[_id][msg.sender] -= _amount;
+        token.transfer(msg.sender, _amount);
+
+        emit Withdraw(_id, msg.sender, _amount);
+    }      
+
+
+    function processFunds(uint _id) external {
+
+        Campaign storage campaign = ongoingCampaigns[_id];
+        require(campaign.creator == msg.sender, "not creator");
+        require(block.timestamp > campaign.endTime, "not ended");
+        require(campaign.currentAmount >= campaign.goal, "current ammount < goal");
+        require(!campaign.claimed, "claimed");
+
+        campaign.claimed = true;
+        token.transfer(campaign.creator, campaign.currentAmount);
+
+        emit ProcessFunds(_id);
+    }
+    // Sends money back only after campaign has finished and campaign did not have sufficent funds
+    function reimburse(uint _id) external {
+        Campaign memory campaign = ongoingCampaigns[_id];
+        require(block.timestamp > campaign.endTime, "not ended");
+        require(campaign.currentAmount < campaign.goal, "pledged >= goal");
+
+        uint bal = potentialDonations[_id][msg.sender];
+        potentialDonations[_id][msg.sender] = 0;
+        token.transfer(msg.sender, bal);
+
+        emit Reimburse(_id, msg.sender, bal);
+    
     }
 
 }

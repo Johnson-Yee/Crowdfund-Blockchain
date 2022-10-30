bf2f51989265cb4970eed2a3b200595e91e3acdc
EthanWong2212 <68017484+EthanWong2212@users.noreply.github.com>
2022-10-30 17:03:50 +0800

    Interaction with smart contract

--- fe/src/Scenes/Home/Home.js
+++ fe/src/Scenes/Home/Home.js
@@ -1,8 +1,15 @@
+import { useMount } from 'ahooks';
 import React from 'react';
 import ProjectCardGrid from '../../Components/ProjectCardGrid/ProjectCardGrid';
 import { MOCK_PROJ_LIST } from '../../Constants/Mocks/MockProjList';
+import { getCampaignCount, getOngoingCampaigns } from '../../Contract/contract';
 
 const Home = () => {
+  useMount(async () => {
+    const campaignCount = await getOngoingCampaigns();
+    console.log(campaignCount);
+  });
+
   return <ProjectCardGrid projectList={MOCK_PROJ_LIST} />;
 };
 

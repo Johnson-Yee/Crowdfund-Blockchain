9378426a1e18dc3409a82d07f9f3ca1961a4a18d
limwenfeng <E0406421@u.nus.edu>
2022-11-01 01:14:58 +0800

    finalize design for project details page

--- fe/src/Scenes/ProjectDetails/ProjectDetails.js
+++ fe/src/Scenes/ProjectDetails/ProjectDetails.js
@@ -7,7 +7,11 @@ import {
   CardMedia,
   LinearProgress,
   Typography,
-  Box
+  Box,
+  Container,
+  Stack,
+  Grid,
+  Button
 } from '@mui/material';
 import { MOCK_PROJ_LIST } from '../../Constants/Mocks/MockProjList';
 
@@ -24,73 +28,80 @@ const ProjectDetails = () => {
   console.log(id);
   console.log(projectdetail);
   return (
-    <>
+    <Grid container spacing={0} direction="column" alignItems="center" justify="center">
       <Card
         className="animate__animated animate__fadeIn"
         raised
         sx={{
           height: 'auto',
+          width: '90%',
           display: 'flex',
           flexDirection: 'column',
-          padding: 2,
-          borderRadius: 5,
+          padding: 0,
+          borderRadius: '0px',
           bgcolor: '#D9D9D9'
         }}>
-        <CardActionArea sx={{ height: '100%', flexGrow: 1 }}>
+        <Grid sx={{ height: '100%', flexGrow: 1 }}>
           <Box sx={{ display: 'flex', flexDirection: 'row' }}>
             <CardMedia
               item="true"
               component="img"
               src={'https://picsum.photos/400/300?random=2'}
-              sx={{ height: '20vw', width: '25vw', objectFit: 'contain', flexGrow: 1 }}
+              sx={{
+                height: '30vw',
+                width: '30vw',
+                objectFit: 'fit',
+                flexGrow: 1,
+                borderRadius: '0px 0px 0px 0px'
+              }}
             />
             <CardContent
-              sx={{ textAlign: 'left', bgcolor: '#D9D9D9', height: 'auto', flexGrow: 2 }}>
-              <Typography gutterBottom variant="h6" noWrap height="15%">
-                Test title
-              </Typography>
-              <Typography
-                gutterBottom
-                variant="body2"
-                color="text.secondary"
-                height="25%"
-                sx={{ textOverflow: 'ellipsis' }}>
-                desc
-              </Typography>
-              <Typography gutterBottom variant="subtitle1" color="text.secondary">
-                By: creator
-              </Typography>
-              <LinearProgress variant="determinate" value={10} />
-              <Typography variant="subtitle1" color="text.secondary">
-                percFunded% funded
-                <br />
-                amtPledged ETH pledged
-                <br />
-                deadline days to go
-              </Typography>
+              sx={{
+                textAlign: 'left',
+                bgcolor: '#D9D9D9',
+                height: 'auto',
+                flexGrow: 2,
+                paddingTop: 0
+              }}>
+              <Stack spacing={1}>
+                <Typography variant="h6" noWrap height="auto" sx={{ paddingTop: 1 }}>
+                  The Dark One Plush
+                </Typography>
+                <LinearProgress variant="determinate" value={10} />
+                <Button variant="contained">Scrap Project And Refund</Button>
+                <Button variant="contained" disabled>
+                  Withdraw Funds to Own Account
+                </Button>
+                <Typography variant="subtitle1" color="text.secondary">
+                  By: Ethan
+                </Typography>
+                <Typography variant="subtitle1" color="text.secondary">
+                  50% funded
+                  <br />
+                  500 ETH pledged
+                  <br />
+                  30 days to go
+                </Typography>
+              </Stack>
             </CardContent>
           </Box>
-          <Typography gutterBottom variant="subtitle1" color="text.secondary">
-            Description
-          </Typography>
-          <Typography gutterBottom variant="subtitle1" color="text.secondary">
-            Description
-          </Typography>
-          <Typography gutterBottom variant="subtitle1" color="text.secondary">
-            Description
-          </Typography>
-          <Typography gutterBottom variant="subtitle1" color="text.secondary">
-            Description
-          </Typography>
-          <Typography gutterBottom variant="subtitle1" color="text.secondary">
-            Description
-          </Typography>
-          <Typography gutterBottom variant="subtitle1" color="text.secondary">
-            Description
-          </Typography>
-        </CardActionArea>
+          <Grid sx={{ padding: 2 }}>
+            <Typography gutterBottom textAlign={'left'} variant="subtitle1" color="text.secondary">
+              Description
+            </Typography>
+            <Typography gutterBottom textAlign={'left'} variant="subtitle1" color="text.secondary">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            </Typography>
+          </Grid>
+          <Grid />
+        </Grid>
       </Card>
-    </>
+    </Grid>
   );
 };
 export default ProjectDetails;
